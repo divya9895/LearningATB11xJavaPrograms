63c99369664da76d610e7644416a361b650fcc05
Divya Bharti <divyabharti9895@gmail.com>
2025-04-30 16:15:52 +0530

    11th March-I'm pushing test code

---
+++ src/ex_01_Java_Basic/LocalFile.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class LocalFile {
+
+    public static void main(String[] args) {
+        System.out.println("Final");
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+}
