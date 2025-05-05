a13a32432300323f06906129ad3e31ceb55811a5
Divya Bharti <divyabharti9895@gmail.com>
2025-05-05 20:02:56 +0530

    11th March-I'm pushing test code

--- src/ex_01_Java_Basics_Part2/Lab020_table_of_9.java
+++ src/ex_01_Java_Basics_Part2/Lab020_table_of_9.java
@@ -3,16 +3,16 @@ package ex_01_Java_Basics_Part2;
 public class Lab020_table_of_9 {
     public static void main(String[] args) {
         int table = 9;
-        System.out.println("9 * 1 = " + (9));
-        System.out.println("9 * 2 = " + (9 * 2));
-        System.out.println("9 * 3 = " + (9 * 3));
-        System.out.println("9 * 4 = " + (9 * 4));
-        System.out.println("9 * 5 = " + (9 * 5));
-        System.out.println("9 * 6 = " + (9 * 6));
-        System.out.println("9 * 7 = " + (9 * 7));
-        System.out.println("9 * 8 = " + (9 * 8));
-        System.out.println("9 * 9 = " + (9 * 9));
-        System.out.println("9 * 10 = " + (9 * 10));
+        System.out.printf("9 * 1 = %d%n", (9));
+        System.out.printf("9 * 2 = %d%n", (9 * 2));
+        System.out.printf("9 * 3 = %d%n", (9 * 3));
+        System.out.printf("9 * 4 = %d%n", (9 * 4));
+        System.out.printf("9 * 5 = %d%n", (9 * 5));
+        System.out.printf("9 * 6 = %d%n", (9 * 6));
+        System.out.printf("9 * 7 = %d%n", (9 * 7));
+        System.out.printf("9 * 8 = %d%n", (9 * 8));
+        System.out.printf("9 * 9 = %d%n", (9 * 9));
+        System.out.printf("9 * 10 = %d%n", (9 * 10));
 
     }
 }
