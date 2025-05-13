c1ae362fbc6ac51ea0dff52cdf140873b321002e
Divya Bharti <divyabharti9895@gmail.com>
2025-05-13 17:25:11 +0530

    11th March-I'm pushing test code

--- src/ex_01_Java_Basics_Part2/Lab023_Multiple_Variables.java
+++ src/ex_01_Java_Basics_Part2/Lab023_Multiple_Variables.java
@@ -3,6 +3,12 @@ package ex_01_Java_Basics_Part2;
 public class Lab023_Multiple_Variables {
     public static void main(String[] args) {
         int a = 10, b = 23, c = 90;
+        int _ = 10;
+
+        int local;
+        local = 10;
+        System.out.println(local);
+
 
     }
 }
