294ef95e8299ae5e7422fc45ebb4e86bd25c3204
Divya Bharti <divyabharti9895@gmail.com>
2025-05-14 19:36:41 +0530

    31st March-I'm pushing test code

---
+++ src/ex_17_OOPs/ATB11x.java
@@ -0,0 +1,20 @@
+package ex_17_OOPs;
+
+public class ATB11x {
+    // Attributes
+    String name;
+    int age;
+    String course;
+    String email;
+    int salary;
+    String gender;
+
+
+
+    // Behaviour
+    void speak(){}
+    void doAssignment(){}
+    void doTask(){}
+    void askQuestion(){}
+    void learnCoding(){}
+}
---
+++ src/ex_17_OOPs/P10.java
@@ -0,0 +1,8 @@
+package ex_17_OOPs;
+
+public class P10 {
+    public static void main(String[] args) {
+        System.out.println("P10");
+    }
+
+}
